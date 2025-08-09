f5602b833a4b5de557bdf3ab55a0a9d57acfa8a0
Vinfall <91039000+Vinfall@users.noreply.github.com>
2025-08-09 18:38:03 +0800

    build: migrate to pyproject.toml
    package via `python3 setup.py sdist`

--- setup.py
+++ setup.py
@@ -18,16 +18,8 @@ setup(
     description="Datasette plugin to deploy on Vercel",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
-    author="Simon Willison, Vinfall",
     url="https://github.com/Vinfall/datasette2vercel",
-    project_urls={
-        "Issues": "https://github.com/Vinfall/datasette2vercel/issues",
-    },
-    license="Apache License, Version 2.0",
     version=VERSION,
     packages=["datasette2vercel"],
     entry_points={"datasette": ["publish_vercel = datasette2vercel"]},
-    install_requires=["datasette>=0.59"],
-    extras_require={"test": ["pytest"]},
-    tests_require=["datasette2vercel[test]"],
 )
